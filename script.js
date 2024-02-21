8facfb94b69d9d2f963d72ca1aee1400990772b0
Emanoel Pontes Timbo <emanoelpontes8@gmail.com>
2024-02-21 18:59:39 -0300

    Correção

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
 };
 
 const getWeatherDetails = (cityName, latitude, longitude) => {
-  const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast/?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=pt_br&units=metric`;
+  const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast/?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=pt_br`;
   //'pt_br&units=metric' esse parâmetro na chave traz tudo em portugues
 
   fetch(WEATHER_API_URL)

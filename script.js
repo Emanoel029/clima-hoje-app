439a1dc14ce38899023447175c9f3e448a64c5d6
Emanoel Pontes Timbo <emanoelpontes8@gmail.com>
2024-02-21 18:04:31 -0300

    Correção da formatação da data

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 const API_KEY = "9f0878c94608b5bc97e84532394af6dd"; //Chave da API para a API do OpenWeatherMap
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const dia = date.getDate().toString().padStart(2, "0"); // adiciona um zero à esquerda se o dia for menor que 10
+  const mes = (date.getMonth() + 1).toString().padStart(2, "0"); // adiciona um zero à esquerda se o mês for menor que 10
+  const ano = date.getFullYear();
+  return `${dia}/${mes}/${ano}`;
+};
+
 const createWeatherCard = (cityName, weatherItem, index) => {
+  const dataFormatada = formatDate(weatherItem.dt_txt.split(" ")[0]);
   if (index === 0) {
     //HTML para o cartão meteorológico principal
     return `<div class="details">
-                <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                <h2>${cityName} (${dataFormatada})</h2>
                 <h6>Teperatura: ${(weatherItem.main.temp - 273.15).toFixed(
                   2
                 )}°c</h6>
@@ -20,17 +30,17 @@ const createWeatherCard = (cityName, weatherItem, index) => {
               <div class="icon">
                 <img src="https://openweathermap.org/img/wn/${
                   weatherItem.weather[0].icon
-                }@4x.png" alt="weather-icon">
+                }@4x.png">
                 <h6>${weatherItem.weather[0].description}</h6>
               </div>
             </div>`;
   } else {
     //HTML para o cartão meteorológico principal
     return `<li class="card">
-              <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+            <h3>(${dataFormatada})</h3>
               <img src="https://openweathermap.org/img/wn/${
                 weatherItem.weather[0].icon
-              }@2x.png" alt="weather-icon">
+              }@2x.png">
               <h6>Teperatura: ${(weatherItem.main.temp - 273.15).toFixed(
                 2
               )}°c</h6>
